333213820ef92f4574ac5a30d231dd21a60480e8
MARCACACAHUATE <marcacacahuate@outlook.com>
2022-10-05 20:26:55 -0500

    views: Ventana principal funcionando con el formulario dummy de registro nuevo

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace codename_boquete.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        // Fields
+        private ViewModelBase _currentChildView;
+
+        // Propiedades
+        public ViewModelBase CurrentChildView
+        {
+            get
+            {
+                return _currentChildView;
+            }
+
+            set
+            {
+                _currentChildView = value;
+                onPropertyChanged(nameof(CurrentChildView));
+            }
+        }
+        // Comandos mamalones
+        public ICommand ShowFormularioViewCommand { get; }
+
+        // Constructor
+        public MainViewModel()
+        {
+            // Inicializamos los comandos para mostrar las vistas
+            ShowFormularioViewCommand = new ViewModelCommand(ExecuteShowFormularioViewCommand);
+
+            // View por Defecto
+            ExecuteShowFormularioViewCommand(null);
+
+        }
+
+
+        // Metodos
+        private void ExecuteShowFormularioViewCommand(object obj)
+        {
+            CurrentChildView = new FormularioViewModel();
+
+        }
+    }
+}
---
+++ ViewModel/ViewModelBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace codename_boquete.ViewModel {
+
+    // Esta clase implementa las notificaciones de cambios de propiedades
+    public abstract class ViewModelBase : INotifyPropertyChanged {
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        // Notifica a los cliente de enlace que un valor de propiedad a cambiado y que debe volver 
+        // a evaluar sus valores.
+        public void onPropertyChanged(string propertyName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+    }
+}
