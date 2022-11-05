d79ccc8cea73328d16dc9b0d88c27dc163e91576
MARCACACAHUATE <marcacacahuate@outlook.com>
2022-11-05 12:15:37 -0600

    views: diseño agregado a la ventana principal

--- View/MainView.xaml.cs
+++ View/MainView.xaml.cs
@@ -23,5 +23,10 @@ namespace codename_boquete.View
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
