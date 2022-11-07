c99f9fb40123e0895e988eab7210b3db543724f0
MARCACACAHUATE <marcacacahuate@outlook.com>
2022-11-07 13:06:55 -0600

    viewmodel: los registros ya se muestran en la vista

--- ViewModel/RegistroViewModel.cs
+++ ViewModel/RegistroViewModel.cs
@@ -9,19 +9,43 @@ namespace codename_boquete.ViewModel
 {
     public class RegistroViewModel: ViewModelBase
     {
+        // Fields
+        private List<CsrfReporteDeFuga> _ListaReporteDeFugas;
+
+
         // Propiedades
+        public List<CsrfReporteDeFuga> ListaReporteDeFugas 
+        {
+            get
+            {
+                return _ListaReporteDeFugas;
+            }
+            set
+            {
+                _ListaReporteDeFugas = value;
+                onPropertyChanged(nameof(ListaReporteDeFugas));
+            } 
+        }
+
 
         // Comandos
 
         // Constructor
+        public RegistroViewModel()
+        {
+            ExecuteListaRegistroViewCommand(null);
+        }
 
         // Metodos
         private void ExecuteListaRegistroViewCommand(object obj)
         {
             using(Model.DESProyectoContext db = new Model.DESProyectoContext())
             {
-
+                List<CsrfReporteDeFuga> lista = (from d in db.CsrfReporteDeFugas
+                             select d).ToList();
+                ListaReporteDeFugas = lista;
             }
+
             
         }
     }
