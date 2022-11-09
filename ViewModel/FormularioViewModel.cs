edce8b3ca0151da34b5443c93e59921e04bb2ae9
MARCACACAHUATE <marcacacahuate@outlook.com>
2022-11-08 20:15:33 -0600

    viewmodel: el binding entre la view y el viewmodel ya jala chido

--- ViewModel/FormularioViewModel.cs
+++ ViewModel/FormularioViewModel.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using codename_boquete.View;
+using codename_boquete.Model;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
 
 namespace codename_boquete.ViewModel
 {
@@ -12,9 +14,16 @@ namespace codename_boquete.ViewModel
     {
         // Fields
         private FugaView _fugaView;
+        private string _numSerie;
+        private string _coilSelect;
+        private bool _coilParaScrap;
+        private bool _fugaFalsa;
+        private int _linea;
+        private int _turno;
+        private string _retrabajadorSelect;
+        private List<string> _retrabajadores;
+        private List<string> _nombreCoil;
 
-        // Comandos
-        public ICommand ShowFugaDetailViewCommand { get; }
 
         // Propiedades
         public FugaView FugaView
@@ -30,17 +39,166 @@ namespace codename_boquete.ViewModel
             }
         }
 
+        public List<string> NombreCoil 
+        {
+            get
+            {
+                return _nombreCoil;
+            }
+
+            set
+            {
+                 _nombreCoil = value;
+                onPropertyChanged(nameof(NombreCoil));
+            }  
+        }
+
+        public List<string> Retrabajadores 
+        {
+            get 
+            { 
+                return _retrabajadores;
+            }
+            set
+            {
+                _retrabajadores = value;
+                onPropertyChanged(nameof(Retrabajadores));
+            }   
+        }
+
+        public string NumSerie 
+        { 
+            get => _numSerie; 
+            set
+            {
+                _numSerie = value;
+                onPropertyChanged(nameof(NumSerie));
+            }  
+        }
+        public bool CoilParaScrap 
+        { 
+            get => _coilParaScrap; 
+            set
+            {
+                _coilParaScrap = value;
+                onPropertyChanged(nameof(CoilParaScrap));
+            } 
+        }
+        public bool FugaFalsa 
+        { 
+            get => _fugaFalsa; 
+            set
+            {
+                _fugaFalsa = value;
+                onPropertyChanged(nameof(FugaFalsa));
+            } 
+        }
+        public int Linea 
+        { 
+            get => _linea; 
+            set
+            {
+                _linea = value;
+                onPropertyChanged(nameof(Linea));
+    
+            } 
+        }
+        public int Turno
+        { 
+            get => _turno; 
+            set
+            {
+                _turno = value;
+                onPropertyChanged(nameof(Turno));
+            } 
+        }
+        public string CoilSelect 
+        { 
+            get => _coilSelect; 
+            set
+            {
+                _coilSelect = value;
+                onPropertyChanged(nameof(CoilSelect));
+            }  
+        }
+        public string RetrabajadorSelect 
+        { 
+            get => _retrabajadorSelect; 
+            set
+            {
+                _retrabajadorSelect = value;
+                onPropertyChanged(nameof(RetrabajadorSelect));
+            }  
+        }
+        // Comandos
+        public ICommand ShowFugaDetailViewCommand { get; }
+        public ICommand CoilScrapViewCommand { get;  }
+        public ICommand FugaFalsaViewCommand { get; }
+        public ICommand LineaViewCommand { get; }
+        public ICommand TurnoViewCommand { get; }
+
         // Constructor
         public FormularioViewModel()
         {
             ShowFugaDetailViewCommand = new ViewModelCommand(ExecuteShowFugaViewCommand);
+            CoilScrapViewCommand = new ViewModelCommand(ExecuteCoilScrapViewCommand);
+            FugaFalsaViewCommand = new ViewModelCommand(ExecuteFugaFalsaViewCommand);
+            LineaViewCommand = new ViewModelCommand(ExecuteLineaViewCommand);
+            TurnoViewCommand = new ViewModelCommand(ExecuteTurnoViewCommand);
+
+
+            ExecuteListCoilsNames(null);
         }
 
         // Metodos
         public void ExecuteShowFugaViewCommand(object obj)
         {
             FugaView = new FugaView();
-            FugaView.Show();
+            FugaView.ShowDialog();
+            System.Diagnostics.Debug.WriteLine("Numero series " + NumSerie);
+            System.Diagnostics.Debug.WriteLine("Coil para Scrap " + CoilParaScrap);
+            System.Diagnostics.Debug.WriteLine("Fuga " + FugaFalsa);
+            System.Diagnostics.Debug.WriteLine("Linea " + Linea);
+            System.Diagnostics.Debug.WriteLine("Turno " + Turno);
+            System.Diagnostics.Debug.WriteLine("retrabajador " + RetrabajadorSelect);
+            System.Diagnostics.Debug.WriteLine("nombre Coil " + CoilSelect);
+        }
+
+        public void ExecuteCoilScrapViewCommand(object obj)
+        {
+
+            CoilParaScrap = (string)obj == "Si" ? true : false;
+        }
+
+        public void ExecuteFugaFalsaViewCommand(object obj)
+        {
+            FugaFalsa = (string)obj == "Si" ? true : false;
+        }
+
+        public void ExecuteLineaViewCommand(object obj)
+        {
+            Linea = int.Parse((string)obj);
+        }
+
+        public void ExecuteTurnoViewCommand(object obj)
+        {
+            Turno = int.Parse((string)obj);
+        }
+
+        public void ExecuteListCoilsNames(object obj)
+        {
+            using(DESProyectoContext db = new DESProyectoContext())
+            {
+                List<string> listModelos = (from data in db.CsrfDataNumbers 
+                                            select data.Modelo).ToList();
+
+                List<string> listRetrabajadores = (from data in db.CsrfRetrabajadores
+                                                   where data.Nombre != null
+                                                   select data.Nombre).ToList();
+
+                NombreCoil = listModelos;
+                Retrabajadores = listRetrabajadores;
+            }
         }
     }
 }
